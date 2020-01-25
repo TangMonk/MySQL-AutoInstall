03bccf33ee8afd7ecbd587d603b0f85704aedbc0
TangMonk <wytwyt02@gmail.com>
2020-01-25 10:27:08 +0800

    TProcess

--- unit1.pas
+++ unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, ShellApi, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  LCLType, windows;
+  LCLType, Windows, process;
 
 type
 
@@ -34,32 +34,28 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   MysqlExeFile: TResourceStream;
-  MysqlSavaToFileSteam: TFileStream;
+  MysqlSavaToFileSteam, ConfigurationProcessStdErr: TFileStream;
   EXEINFO: _SHELLEXECUTEINFOA;
   ConfigEXEINFO: _SHELLEXECUTEINFOA;
+  MySQLInstallProcess, ConfigurationProcess: TProcess;
 begin
-  Button1.Caption:='安装中...';
-  Button1.Enabled:=false;
-  MysqlExeFile:=TResourceStream.Create(HINSTANCE, 'MYSQL-5.5.60-WINX64', RT_RCDATA);
-  MysqlSavaToFileSteam:=TFileStream.Create( GetTempDir() + '.\MYSQL.msi', fmCreate);
+  Button1.Caption := '安装中...';
+  Button1.Enabled := False;
+  MysqlExeFile := TResourceStream.Create(HINSTANCE, 'MYSQL-5.5.60-WINX64', RT_RCDATA);
+  MysqlSavaToFileSteam := TFileStream.Create(GetTempDir() + '.\MYSQL.msi', fmCreate);
   MysqlSavaToFileSteam.CopyFrom(MysqlExeFile, 0);
   MysqlSavaToFileSteam.Write(MysqlExeFile.Memory, MysqlExeFile.Size);
   MysqlSavaToFileSteam.Free;
-  with EXEINFO do
-  begin
-    cbSize:=SizeOf(EXEINFO);
-    fMask:=SEE_MASK_NOCLOSEPROCESS;
-    wnd:=0;
-    lpVerb:=nil;
-    lpFile:='msiexec.exe';
-    lpParameters:='/i C:\Users\wuao\AppData\Local\Temp\mysql-5.5.60-winx64.msi /passive';
-    lpDirectory:=nil;
-    nShow:=SW_SHOW;
-    hInstApp:=0;
 
-  end;
-  ShellExecuteExA(@EXEINFO);
-  WaitForSingleObject(EXEINFO.hProcess, INFINITE);
+  MySQLInstallProcess := TProcess.Create(nil);
+  MySQLInstallProcess.Executable := 'msiexec.exe';
+  MySQLInstallProcess.Parameters.Add('/a');
+  MySQLInstallProcess.Parameters.Add('C:\Users\wuao\AppData\Local\Temp\MYSQL.msi');
+  MySQLInstallProcess.Parameters.Add('INSTALLFOLDER="C:\MySQL\"');
+  MySQLInstallProcess.Parameters.Add('/passive');
+  MySQLInstallProcess.Options := MySQLInstallProcess.Options + [poWaitOnExit];
+  MySQLInstallProcess.Execute;
+  MySQLInstallProcess.Free;
 
   with ConfigEXEINFO do
   begin
@@ -67,20 +63,18 @@ begin
     fMask:=SEE_MASK_NOCLOSEPROCESS;
     wnd:=0;
     lpVerb:=nil;
-    lpFile:='MySQLInstanceConfig.exe';
-    lpParameters:='-i -q  "-lC:\mysql_install_log.txt" "-nMySQL Server 5.5"  "-pC:\Program Files\MySQL\MySQL Server 5.5" -v 5.5.60  ServerType=DEVELOPMENT DatabaseType=MIXED   ConnectionUsage=DSS Port=3306 ServiceName=MySQL5.5 RootPassword=root ConnectionCount=50 Charset=gbk';
-    lpDirectory:='C:\Program Files\MySQL\MySQL Server 5.5\bin';
+    lpFile:='.\MySQLInstanceConfig.exe';
+    lpParameters:='-i -q  "-lC:\mysql_install_log.txt" "-nMySQL Server 5.5"  "-pC:\Program Files\MySQL\MySQL Server 5.5" -v 5.5.60  ServerType=DEVELOPMENT DatabaseType=MIXED   ConnectionUsage=DSS Port=3306 ServiceName=MySQL55 RootPassword=root ConnectionCount=50 Charset=gbk';
+    lpDirectory:='C:\MySQL\MySQL\MySQL Server 5.5\bin';
     nShow:=SW_HIDE;
     hInstApp:=0;
   end;
 
   ShellExecuteExA(@ConfigEXEINFO);
-  Sleep(5000);
-  //WaitForSingleObject(ConfigEXEINFO.hProcess, INFINITE);
+  WaitForSingleObject(ConfigEXEINFO.hProcess, 5000);
   CloseHandle(ConfigEXEINFO.hProcess);
-  CloseHandle(EXEINFO.hProcess);
-  Button1.Enabled:=true;
-  Button1.Caption:='安装';
+  Button1.Enabled := True;
+  Button1.Caption := '安装';
   ShowMessage('安装完成');
 end;
 
